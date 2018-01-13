09822abf04dc0651f2c1f2d18f4cf28db2def023
vinoth525 <vinosssvino@gmail.com>
2018-01-13 16:47:34 +0530

    initial connect and disconnection setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO  = require('socket.io');
 
 var app = express();
 const publicPath = path.join(__dirname, '../public');
-const port = express.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () =>{
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
+
+
+
+server.listen(port, () =>{
     console.log(`The server is up on port ${port}`);
 })
 
